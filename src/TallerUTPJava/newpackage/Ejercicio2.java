3d5e51a8f75afdc06ce28adfafa66e8ce9c90a83
USER <USER@DESKTOP-DEB2DNR>
2025-08-16 11:21:54 -0500

    Ejemplo

---
+++ src/TallerUTPJava/newpackage/Ejercicio2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TallerUTPJava.newpackage;
+
+/**
+ *
+ * @author USER
+ */
+public class Ejercicio2 {
+     public static void main(String[]args){
+         String nombre = "dennys";
+         int edad = 31;
+         boolean esEstudiante = true;
+         System.out.println("Nombre: " + nombre);
+         System.out.println("Edad:" + edad);
+         System.out.println("¿es estudiante?:" + esEstudiante);
+         
+     }
+    
+}
