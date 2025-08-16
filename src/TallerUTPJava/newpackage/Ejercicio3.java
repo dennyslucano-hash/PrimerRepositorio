085bce5d42b33c1a0f8109f1ff9c7ab50d0575eb
USER <USER@DESKTOP-DEB2DNR>
2025-08-16 11:29:33 -0500

    Ejemplo

---
+++ src/TallerUTPJava/newpackage/Ejercicio3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TallerUTPJava.newpackage;
+
+/**
+ *
+ * @author USER
+ */
+public class Ejercicio3 {
+    public static void main(String[]args){
+        final double PI = 3.1416;
+        double radio = 5;
+        double area = PI * radio * radio;
+        System.out.println("Areadel circulo: " + area);
+       
+        
+    }
+}
