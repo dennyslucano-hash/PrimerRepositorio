ddf8aba6e61f22af8fdbc7abed2d1e3b045cd30c
USER <USER@DESKTOP-DEB2DNR>
2025-08-16 10:58:50 -0500

    Ejemplo

---
+++ src/TallerUTPJava/newpackage/MiprimerPrograma.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TallerUTPJava.newpackage;
+
+/**
+ *
+ * @author USER
+ */
+public class MiprimerPrograma {
+    /**
+     * metodo principal que se ejecuta al iniciar el rograma.
+     * Dentro de este metodo se usan instrucciones para imprimir texto en consola.
+     * 
+     * @param args 
+     */
+  public static void main(String[]args){
+      //Imprimir el nombre del usuario
+      System.out.println("Mi nombre es Dennys");
+      //Imprimir la edad del usuario
+      System.out.println("Mi nombre es Dennys");
+  }  
+}
