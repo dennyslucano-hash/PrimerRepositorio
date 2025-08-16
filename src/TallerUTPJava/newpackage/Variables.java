5762c43c3017e4113cd775178d6f27fb4787b786
USER <USER@DESKTOP-DEB2DNR>
2025-08-16 08:59:07 -0500

    Ejemplo

---
+++ src/TallerUTPJava/newpackage/Variables.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TallerUTPJava.newpackage;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class Variables {
+    public static void main(String[]args){
+System.out.println("Hola, mundo!");
+Scanner scanner = new Scanner(System.in);
+System.out.print("Ingresa tu nombre: ");
+String nombre = scanner.nextLine();
+System.out.println("Tu nombre es: " + nombre);
+}
+    }
+
