59248aa9ceb7472d7818862e220a82e84df6e8fa
USER <USER@DESKTOP-DEB2DNR>
2025-08-20 20:26:35 -0500

    Ejemplo

--- src/TareaSemana1/Ejercicio2.java
+++ src/TareaSemana1/Ejercicio2.java
@@ -1,13 +1,10 @@
-package TareaSemana1;
-
-
-import *;
-import java.util.Scanner;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package TareaSemana1;
+
+import java.util.Scanner;
 
 /**
  *
